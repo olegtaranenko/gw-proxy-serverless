9402a7142718341ff5860d57e84ad11fc359b780
Andriy Tykhonov <atykhonov@gmail.com>
2020-03-27 18:16:28 +0200

    Return 200 http status even in a case of error.

--- pagerduty-jira-synchronization/app.py
+++ pagerduty-jira-synchronization/app.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from flask import Flask, jsonify, request
 
@@ -6,11 +7,20 @@ import webhooks
 
 
 app = Flask(__name__)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 @app.route("/pagerduty-webhook", methods=["POST"])
 def pagerduty_webhook():
-    webhooks.pagerduty(request.json)
-    return jsonify({
-        'ok': True,
-    })
+    response = {'ok': True}
+    try:
+        webhooks.pagerduty(request.json)
+    except Exception as e:
+        logger.exception(
+            'Error occured during processing of a PagerDuty webhook')
+        response = {
+            'ok': False,
+            'error': repr(e),
+        }
+    return jsonify(response)
