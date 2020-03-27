2e80a7028eb619cb3a8c31ffdcb2516ca98de6b3
Andriy Tykhonov <atykhonov@gmail.com>
2020-03-27 22:56:02 +0200

    Rename `itemId` to `itemKey` and fix issue resolving.
    Jira issue wasn't resolved when PagerDuty incident is resolved. These
    changes fixes this.

--- pagerduty-jira-synchronization/webhooks.py
+++ pagerduty-jira-synchronization/webhooks.py
@@ -32,16 +32,19 @@ def handle_triggered_incident(message):
         if severity_field_id:
             issue_dict[severity_field_id] = {'value': severity_field_value}
         issue = jira.create_issue(fields=issue_dict)
-        db.put_incident_issue_relation(incident['id'], issue.id)
+        db.put_incident_issue_relation(incident['id'], issue.key)
 
 
 def handle_resolved_incident(message):
     incident = message.get('incident')
-    issue_id = db.get_issue_by_incident_id(incident['id'])
-    if issue_id is not None:
+    issue_key = db.get_issue_key_by_incident_id(incident['id'])
+    if issue_key is not None:
         jira = utils.get_jira()
-        issue = jira.issue(issue_id)
-        jira.transition_issue(issue, '5', resolution={'name': 'Done'})
+        issue = jira.issue(issue_key)
+        done_transition_ids = [
+            t['id'] for t in jira.transitions(issue) if t['name'] == 'Done']
+        if done_transition_ids:
+            jira.transition_issue(issue, done_transition_ids[0])
 
 
 def pagerduty(event):
